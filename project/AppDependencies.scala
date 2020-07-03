80263166d3c545bf14e89332c34c80e34f4c532a
Jay Nabonne <jaynabonne@gmail.com>
2020-07-03 14:06:43 +0100

    Removed Mongo.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,6 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "org.reactivemongo" %% "play2-reactivemongo"            % "0.18.6-play26",
     "uk.gov.hmrc"       %% "logback-json-logger"            % "4.6.0",
     "uk.gov.hmrc"       %% "govuk-template"                 % "5.52.0-play-26",
     "uk.gov.hmrc"       %% "play-health"                    % "3.14.0-play-26",
