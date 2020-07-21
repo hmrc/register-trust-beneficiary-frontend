6d3e176ad85dd0107a2f50a85635004098765db4
Jay Nabonne <jaynabonne@gmail.com>
2020-07-21 15:36:12 +0100

    TRUS-2195: Company beneficiary share of income page.

---
+++ test/views/register/beneficiaries/companyoremploymentrelated/company/ShareOfIncomeViewSpec.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.register.beneficiaries.companyoremploymentrelated.company
+
+import forms.IncomePercentageFormProvider
+import models.NormalMode
+import play.api.data.Form
+import play.twirl.api.HtmlFormat
+import views.behaviours.{IntViewBehaviours, QuestionViewBehaviours}
+import views.html.register.beneficiaries.companyoremploymentrelated.company.ShareOfIncomeView
+
+class ShareOfIncomeViewSpec extends IntViewBehaviours {
+
+  private val index = 0
+  val messageKeyPrefix = "companyBeneficiary.shareOfIncome"
+  val name: String = "Company"
+
+  val form: Form[Int] = new IncomePercentageFormProvider().withPrefix(messageKeyPrefix)
+  val view: ShareOfIncomeView = viewFor[ShareOfIncomeView](Some(emptyUserAnswers))
+
+  "ShareOfIncome view" must {
+
+    def applyView(form: Form[_]): HtmlFormat.Appendable =
+      view.apply(form, NormalMode, name, index, draftId)(fakeRequest, messages)
+
+    behave like dynamicTitlePage(applyView(form), messageKeyPrefix, name)
+
+    behave like pageWithBackLink(applyView(form))
+
+    behave like intPage(form, applyView, messageKeyPrefix, name)
+
+    behave like pageWithASubmitButton(applyView(form))
+  }
+
+}
