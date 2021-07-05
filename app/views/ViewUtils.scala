f67e615e1e8d8bc7ecf63017a28c5e11055b6984
darrylmurphy <darrylmurphy@users.noreply.github.com>
2021-07-05 14:15:34 +0100

    Convet radio options

--- app/views/ViewUtils.scala
+++ app/views/ViewUtils.scala
@@ -18,6 +18,8 @@ package views
 
 import play.api.data.{Form, FormError}
 import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.html.components.{RadioItem, Text}
+import viewmodels.RadioOption
 
 object ViewUtils {
 
@@ -44,4 +46,19 @@ object ViewUtils {
   def breadcrumbTitle(title: String)(implicit messages: Messages): String = {
     s"$title - ${messages("entity.beneficiary")} - ${messages("site.service_name")} - GOV.UK"
   }
+
+  def mapRadioOptionsToRadioItems(field: Field, trackGa: Boolean,
+                                  inputs: Seq[RadioOption])(implicit messages: Messages): Seq[RadioItem] =
+    inputs.map(
+      a => {
+        RadioItem(
+          id = Some(a.id),
+          value = Some(a.value),
+          checked = field.value.contains(a.value),
+          content = Text(messages(a.messageKey)),
+          attributes = if (trackGa) Map[String, String]("data-journey-click" -> s"trusts-frontend:click:${a.id}") else Map.empty
+        )
+      }
+    )
+
 }
