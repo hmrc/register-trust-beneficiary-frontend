c39a293983acf676c5beadc9b78f4c37ddbf5e0b
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2021-07-12 15:52:41 +0100

    [CS] CheckYourAnswers update / tidy up

--- app/controllers/register/beneficiaries/charityortrust/trust/AnswersController.scala
+++ app/controllers/register/beneficiaries/charityortrust/trust/AnswersController.scala
@@ -49,7 +49,7 @@ class AnswersController @Inject()(
     implicit request =>
 
       val section: AnswerSection = printHelper.checkDetailsSection(request.userAnswers, request.beneficiaryName, index, draftId)
-      Ok(view(section, index, draftId))
+      Ok(view(Seq(section), index, draftId))
   }
 
   def onSubmit(index: Int, draftId: String): Action[AnyContent] = standardActionSets.identifiedUserWithData(draftId).async {
