b1822a5a4bc51fc4e008f9aeda4264516a3983ed
slavvco <13876870+slavvco@users.noreply.github.com>
2024-01-17 10:18:23 +0000

    DDCE-5069 | Updated error messages.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.22.0"
+  val bootstrapVersion = "7.23.0"
 
   private lazy val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.23.0-play-28",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.29.0-play-28",
     "uk.gov.hmrc"             %% "domain"                         % "8.3.0-play-28",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % bootstrapVersion,
@@ -15,11 +15,11 @@ object AppDependencies {
 
   private lazy val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapVersion,
-    "org.jsoup"                   %  "jsoup"                    % "1.16.1",
+    "org.jsoup"                   %  "jsoup"                    % "1.17.2",
     "org.scalatest"               %% "scalatest"                % "3.2.17",
     "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.17.0",
-    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.27",
-    "org.wiremock"                %  "wiremock-standalone"      % "3.2.0",
+    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.30",
+    "org.wiremock"                %  "wiremock-standalone"      % "3.3.1",
     "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
   ).map(_ % Test)
