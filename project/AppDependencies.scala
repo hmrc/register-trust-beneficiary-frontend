e66db219bd9a7a3fc01c7c1ce392c01a7c99d176
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-10-10 13:16:55 +0100

    DDCE-7507 - updating play frontend dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.19.0"
+  val bootstrapVersion = "10.2.0"
 
   private lazy val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "9.11.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.3.0",
     "uk.gov.hmrc"             %% "domain-play-30"                         % "11.0.0",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"             % bootstrapVersion,
@@ -15,12 +15,10 @@ object AppDependencies {
 
   private lazy val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-30"   % bootstrapVersion,
-    "org.jsoup"                   %  "jsoup"                    % "1.21.1",
     "org.scalatest"               %% "scalatest"                % "3.2.19",
     "org.scalatestplus"           %% "scalacheck-1-18"          % "3.2.19.0",
     "org.wiremock"                %  "wiremock-standalone"      % "3.13.1",
-    "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
-    "com.vladsch.flexmark"         %  "flexmark-all"              % "0.64.8"
+    "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
