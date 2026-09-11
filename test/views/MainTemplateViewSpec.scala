ac2395320e5a7390406ef69a4adac9a898979a9b
amruta-lohote <203861386+amruta-lohote@users.noreply.github.com>
2026-09-11 11:32:32 +0100

    DDCE-9178-scalafmtCheck

--- test/views/MainTemplateViewSpec.scala
+++ test/views/MainTemplateViewSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package views
 
 import play.twirl.api.{Html, HtmlFormat}
@@ -6,9 +22,9 @@ import views.html.MainTemplate
 
 import scala.jdk.CollectionConverters._
 
-class MainTemplateViewSpec extends ViewBehaviours  {
+class MainTemplateViewSpec extends ViewBehaviours {
 
-  val title = "Test page"
+  val title       = "Test page"
   val mainContent = Html("<p id='main-content'>hello</p>")
 
   private val view: MainTemplate = injector.instanceOf[MainTemplate]
@@ -72,4 +88,5 @@ class MainTemplateViewSpec extends ViewBehaviours  {
       doc.select("meta[name=hmrc-timeout-dialog]").isEmpty mustBe true
     }
   }
+
 }
